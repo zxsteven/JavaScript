a70f599c14aa89941e496e4028d0cbb3ac027da3
Steven Buxton <breathesteve@gmail.com>
2016-01-14 19:21:18 -0600

    arrays

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-var newText = document.getElementById('input').value;
+var myArray=new Array();
+
+ myArray[0]="pizza";
+ myArray[1]="chocolate";
+
+ myOtherArray=["rain","sunshine"];
+
+ myOtherArray.push("snow");
+
+ myOtherArray.splice(2, 0, "hail", "cloudy");
+
+ console.log(myOtherArray.length);
 
 document.getElementById('button').onclick = function() {
-	newText = document.getElementById('input').value;
 	document.getElementById('firstDiv').innerHTML = newText;
 }
