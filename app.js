f7a7f838b18f2d3383cef9d8105f670bf9168bcc
Steven Buxton <breathesteve@gmail.com>
2016-01-14 23:13:11 -0600

    finished.

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 var doc = document.getElementById('box');
 var createdTime, clickedTime, reactionTime;
 
@@ -7,6 +16,19 @@ function makeBox() {
 		time = time * 5000;
 
 	setTimeout(function() {
+		if (Math.random() > 0.5) {
+			doc.style.borderRadius = '100px';
+		} else {
+			doc.borderRadius = '0px';
+		}
+		var top = Math.random();
+			top = top * 400;
+		var left = Math.random();
+			left = left * 600;
+
+		doc.style.top = top + 'px';
+		doc.style.left = left + 'px';
+		doc.style.backgroundColor = getRandomColor();
 		doc.style.display = 'block';
 		createdTime = Date.now();
 	}, time);
